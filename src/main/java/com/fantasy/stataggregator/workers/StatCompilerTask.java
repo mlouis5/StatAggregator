7d5a9b9e9482201a914e6b75d352167d197dd02d
mlouis5 <macdersonlouis@gmail.com>
2014-10-29 23:44:37 -0400

    Created new class to implement stat compiler task

---
+++ src/main/java/com/fantasy/stataggregator/workers/StatCompilerTask.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.fantasy.stataggregator.workers;
+
+import com.fantasy.stataggregator.Task;
+
+/**
+ * StatCompilerTask compiles game statistics into whole season statistics for each player<br>
+ * who played within each individual game.
+ * 
+ * @version %I%, %G%
+ * @author Mac
+ */
+public class StatCompilerTask implements Task{
+
+    @Override
+    public boolean taskComplete() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void run() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
